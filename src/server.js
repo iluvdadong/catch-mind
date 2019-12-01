541f76320c1719832ca796e383e503b88362773e
Dahae Kim <dh77dh77@gmail.com>
2019-12-01 17:09:11 +0900

    Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -10,18 +10,21 @@ app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
-const handleListening = () => 
-    console.log('Server running: http://localhost:${PORT}');
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
-
 const io = socketIO.listen(server);
 
-let sockets = []
-
 io.on("connection", socket => {
-    sockets.push(socket.id);
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon"
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
-
-setInterval(() => console.log(sockets),1000);
-
