d0105fcb4cd2898ea947e6e20a75c10ee826d51e
Dahae Kim <dh77dh77@gmail.com>
2019-12-16 22:47:25 +0900

    #4.2 Global Variables

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,8 +9,9 @@ const LOGGED_IN = "loggedIn";
 const nickname = localStorage.getItem(NICKNAME);
 
 const logIn = nickname => {
-    const socket = io("/");
-    socket.emit("setNickname", { nickname });
+  
+    window.socket = io("/");
+    window.socket.emit(window.events.setNickname, { nickname });
 };
 
 
